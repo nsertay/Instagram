661ca634bd57d20403c9201be28a84dc623c7b86
nurmukhanbet <nurmukhanbetsertay@gmail.com>
2023-05-11 11:59:58 +0600

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Nurmukhanbet Sertay on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
